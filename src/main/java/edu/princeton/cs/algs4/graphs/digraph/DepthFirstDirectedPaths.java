68a2e42dde8f3a44c59150ef1e97d6f02f15b504
Jefferson Song <jeffersonsong@yahoo.com>
2020-12-25 15:47:29 -0500

    Code refactoring.

--- src/main/java/edu/princeton/cs/algs4/graphs/digraph/DepthFirstDirectedPaths.java
+++ src/main/java/edu/princeton/cs/algs4/graphs/digraph/DepthFirstDirectedPaths.java
@@ -29,12 +29,9 @@
 
 package edu.princeton.cs.algs4.graphs.digraph;
 
-import edu.princeton.cs.algs4.fundamentals.stack.LinkedStack;
+import edu.princeton.cs.algs4.graphs.graph.DepthFirstPaths;
 import edu.princeton.cs.algs4.utils.io.In;
-import edu.princeton.cs.algs4.fundamentals.stack.Stack;
 import edu.princeton.cs.algs4.utils.io.StdOut;
-
-import static edu.princeton.cs.algs4.utils.PreConditions.checkArgument;
 
 /**
  *  The {@code DepthFirstDirectedPaths} class represents a data type for
@@ -56,10 +53,7 @@ import static edu.princeton.cs.algs4.utils.PreConditions.checkArgument;
  *  @author Robert Sedgewick
  *  @author Kevin Wayne
  */
-public class DepthFirstDirectedPaths {
-    private final boolean[] marked;  // marked[v] = true iff v is reachable from s
-    private final int[] edgeTo;      // edgeTo[v] = last edge on path from s to v
-    private final int s;       // source vertex
+public class DepthFirstDirectedPaths extends DepthFirstPaths {
 
     /**
      * Computes a directed path from {@code s} to every other vertex in digraph {@code G}.
@@ -68,58 +62,7 @@ public class DepthFirstDirectedPaths {
      * @throws IllegalArgumentException unless {@code 0 <= s < V}
      */
     public DepthFirstDirectedPaths(Digraph G, int s) {
-        marked = new boolean[G.V()];
-        edgeTo = new int[G.V()];
-        this.s = s;
-        validateVertex(s);
-        dfs(G, s);
-    }
-
-    private void dfs(Digraph G, int v) { 
-        marked[v] = true;
-        for (int w : G.adj(v)) {
-            if (!marked[w]) {
-                edgeTo[w] = v;
-                dfs(G, w);
-            }
-        }
-    }
-
-    /**
-     * Is there a directed path from the source vertex {@code s} to vertex {@code v}?
-     * @param  v the vertex
-     * @return {@code true} if there is a directed path from the source
-     *         vertex {@code s} to vertex {@code v}, {@code false} otherwise
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
-     */
-    public boolean hasPathTo(int v) {
-        validateVertex(v);
-        return marked[v];
-    }
-
-    
-    /**
-     * Returns a directed path from the source vertex {@code s} to vertex {@code v}, or
-     * {@code null} if no such path.
-     * @param  v the vertex
-     * @return the sequence of vertices on a directed path from the source vertex
-     *         {@code s} to vertex {@code v}, as an Iterable
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
-     */
-    public Iterable<Integer> pathTo(int v) {
-        validateVertex(v);
-        if (!hasPathTo(v)) return null;
-        Stack<Integer> path = new LinkedStack<>();
-        for (int x = v; x != s; x = edgeTo[x])
-            path.push(x);
-        path.push(s);
-        return path;
-    }
-
-    // throw an IllegalArgumentException unless {@code 0 <= v < V}
-    private void validateVertex(int v) {
-        int V = marked.length;
-        checkArgument(v >= 0 && v < V, "vertex " + v + " is not between 0 and " + (V-1));
+        super(G, s);
     }
 
     /**
