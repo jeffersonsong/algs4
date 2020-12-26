ccb584d6f985c8947cf5d084a3855396b61bc3f9
Jefferson Song <jeffersonsong@yahoo.com>
2020-12-25 20:37:03 -0500

    Code refactoring.

--- src/main/java/edu/princeton/cs/algs4/sorting/pq/IndexPQ.java
+++ src/main/java/edu/princeton/cs/algs4/sorting/pq/IndexPQ.java
@@ -1,23 +1,107 @@
 package edu.princeton.cs.algs4.sorting.pq;
 
+import java.util.NoSuchElementException;
+
+/**
+ *  The {@code IndexMinPQ} class represents an indexed priority queue of generic keys.
+ *  It supports the usual <em>insert</em> and <em>delete-the-minimum</em>
+ *  operations, along with <em>delete</em> and <em>change-the-key</em>
+ *  methods. In order to let the client refer to keys on the priority queue,
+ *  an integer between {@code 0} and {@code maxN - 1}
+ *  is associated with each key—the client uses this integer to specify
+ *  which key to delete or change.
+ *  It also supports methods for peeking at the minimum key,
+ *  testing if the priority queue is empty, and iterating through
+ *  the keys.
+ *
+ *  @param <Key> the generic type of key on this priority queue
+ */
 public interface IndexPQ<Key> extends Iterable<Integer> {
+    /**
+     * Associates key with index {@code i}.
+     *
+     * @param  i an index
+     * @param  key the key to associate with index {@code i}
+     * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
+     * @throws IllegalArgumentException if there already is an item associated
+     *         with index {@code i}
+     */
     void insert(int i, Key key);
 
+    /**
+     * Returns the key associated with index {@code i}.
+     *
+     * @param  i the index of the key to return
+     * @return the key associated with index {@code i}
+     * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
+     * @throws NoSuchElementException no key is associated with index {@code i}
+     */
     Key keyOf(int i);
 
-    void delete(int i);
-
+    /**
+     * Removes a minimum key and returns its associated index.
+     * @return an index associated with a minimum key
+     * @throws NoSuchElementException if this priority queue is empty
+     */
     int poll();
 
+    /**
+     * Remove the key associated with index {@code i}.
+     *
+     * @param  i the index of the key to remove
+     * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
+     * @throws NoSuchElementException no key is associated with index {@code i}
+     */
+    void delete(int i);
+
+    /**
+     * Returns an index associated with a minimum key.
+     *
+     * @return an index associated with a minimum key
+     * @throws NoSuchElementException if this priority queue is empty
+     */
     int peek();
 
-    void changeKey(int i, Key key);
-
-    boolean contains(int i);
-
+    /**
+     * Returns a minimum key.
+     *
+     * @return a minimum key
+     * @throws NoSuchElementException if this priority queue is empty
+     */
     Key peekKey();
 
+    /**
+     * Change the key associated with index {@code i} to the specified value.
+     *
+     * @param  i the index of the key to change
+     * @param  key change the key associated with index {@code i} to this key
+     * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
+     * @throws NoSuchElementException no key is associated with index {@code i}
+     */
+    void changeKey(int i, Key key);
+
+    /**
+     * Is {@code i} an index on this priority queue?
+     *
+     * @param  i an index
+     * @return {@code true} if {@code i} is an index on this priority queue;
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
+     */
+    boolean contains(int i);
+
+    /**
+     * Returns true if this priority queue is empty.
+     *
+     * @return {@code true} if this priority queue is empty;
+     *         {@code false} otherwise
+     */
     boolean isEmpty();
 
+    /**
+     * Returns the number of keys on this priority queue.
+     *
+     * @return the number of keys on this priority queue
+     */
     int size();
 }
--- src/main/java/edu/princeton/cs/algs4/sorting/pq/PQ.java
+++ src/main/java/edu/princeton/cs/algs4/sorting/pq/PQ.java
@@ -4,7 +4,8 @@ package edu.princeton.cs.algs4.sorting.pq;
  * The elements of the priority queue are ordered according to their natural ordering, or by a Comparator provided at
  * queue construction time, depending on which constructor is used. A priority queue does not permit null elements.
  * A priority queue relying on natural ordering also does not permit insertion of non-comparable objects
- * @param <Key>
+ *
+ * @param <Key> the generic type of key on this priority queue
  */
 public interface PQ<Key> extends Iterable<Key> {
     /**
@@ -27,7 +28,18 @@ public interface PQ<Key> extends Iterable<Key> {
      */
     Key peek();
 
+    /**
+     * Returns true if this priority queue is empty.
+     *
+     * @return {@code true} if this priority queue is empty;
+     *         {@code false} otherwise
+     */
     boolean isEmpty();
 
+    /**
+     * Returns the number of keys on this priority queue.
+     *
+     * @return the number of keys on this priority queue
+     */
     int size();
 }
