a831c1b4b2e570dd3e8e32df8ae730aa5ab35ff9
Jefferson Song <jeffersonsong@yahoo.com>
2020-12-26 01:32:08 -0500

    Add Eulerian path & circle.

---
+++ src/test/java/edu/princeton/cs/algs4/graphs/digraph/DirectedEulerianPathTest.java
@@ -0,0 +1,21 @@
+package edu.princeton.cs.algs4.graphs.digraph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DirectedEulerianPathTest {
+    @Test
+    public void testEulerianCycle() {
+        Digraph g = new DigraphImpl(5);
+        g.addEdge(1, 0);
+        g.addEdge(0, 2);
+        g.addEdge(2, 1);
+        g.addEdge(0, 3);
+        g.addEdge(3, 4);
+//        g.addEdge(4, 0);
+
+        DirectedEulerianPath eulerian = new DirectedEulerianPath(g);
+        assertTrue(eulerian.hasEulerianPath());
+    }
+}
