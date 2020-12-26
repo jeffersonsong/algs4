49becf7b13cc554b47fcf47aa1ae263dc86911ce
Jefferson Song <jeffersonsong@yahoo.com>
2020-12-26 01:09:12 -0500

    Add Eulerian path & circle.

---
+++ src/main/java/edu/princeton/cs/algs4/graphs/digraph/KosarajuSC.java
@@ -0,0 +1,37 @@
+package edu.princeton.cs.algs4.graphs.digraph;
+
+import edu.princeton.cs.algs4.graphs.digraph.Digraph;
+import edu.princeton.cs.algs4.graphs.graph.DepthFirstSearch;
+
+/**
+ * Given a directed graph, find out whether the graph is strongly connected or not. A directed graph is strongly
+ * connected if there is a path between any two pair of vertices.
+ *
+ * Kosaraju’s DFS based simple algorithm that does two DFS traversals of graph:
+ *
+ * 1) Initialize all vertices as not visited.
+ * 2) Do a DFS traversal of graph starting from any arbitrary vertex v. If DFS traversal doesn’t visit all vertices,
+ * then return false.
+ * 3) Reverse all arcs (or find transpose or reverse of graph)
+ * 4) Mark all vertices as not-visited in reversed graph.
+ * 5) Do a DFS traversal of reversed graph starting from same vertex v (Same as step 2). If DFS traversal doesn’t
+ * visit all vertices, then return false. Otherwise return true.
+ */
+public class KosarajuSC {
+    /**
+     * @return true if the graph is strongly connected.
+     */
+    public static boolean isSC(Digraph G) {
+        return allVerticesReachable(G, 0) &&
+                allVerticesReachable(G.reverse(), 0);
+    }
+
+    private static boolean allVerticesReachable(Digraph G, int s) {
+        // If DFS traversal doesn't visit all vertices, then return false.
+        DepthFirstSearch dfs = new DepthFirstSearch(G, s);
+        for (int v = 0; v < G.V(); v++) {
+            if (!dfs.marked(v)) return false;
+        }
+        return true;
+    }
+}
--- src/main/java/edu/princeton/cs/algs4/graphs/digraph/eulerian/DirectedEulerianCycle.java
+++ src/main/java/edu/princeton/cs/algs4/graphs/digraph/eulerian/DirectedEulerianCycle.java
@@ -1,8 +1,11 @@
 package edu.princeton.cs.algs4.graphs.digraph.eulerian;
 
 import edu.princeton.cs.algs4.graphs.digraph.Digraph;
-import edu.princeton.cs.algs4.graphs.graph.DepthFirstSearch;
+import edu.princeton.cs.algs4.graphs.digraph.KosarajuSC;
 
+/**
+ * @author Princi Singh
+ */
 public class DirectedEulerianCycle {
 
     /**
@@ -12,7 +15,7 @@ public class DirectedEulerianCycle {
      */
     public boolean isEulerianCycle(Digraph G) {
         // Check if all non-zero degree vertices are connected
-        if (!isSC(G)) return false;
+        if (!KosarajuSC.isSC(G)) return false;
 
         // Check if in degree and out degree of every vertex is same
         for (int v = 0; v < G.V(); v++) {
@@ -21,22 +24,4 @@ public class DirectedEulerianCycle {
 
         return true;
     }
-
-    /**
-     * @return true if the graph is strongly connected.
-     */
-    private boolean isSC(Digraph G) {
-        return allVerticesReachable(G, 0) &&
-                allVerticesReachable(G.reverse(), 0);
-    }
-
-    private boolean allVerticesReachable(Digraph G, int s) {
-        // If DFS traversal doesn't visit all vertices, then return false.
-        DepthFirstSearch dfs = new DepthFirstSearch(G, s);
-        for (int v = 0; v < G.V(); v++) {
-            if (!dfs.marked(v)) return false;
-        }
-        return true;
-    }
-
 }
