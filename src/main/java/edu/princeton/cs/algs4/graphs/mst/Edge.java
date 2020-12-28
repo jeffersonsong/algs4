d3825a0a54d5fa77034640a08de8972c75b88e10
Jefferson Song <jeffersonsong@yahoo.com>
2020-12-27 23:14:54 -0500

    Code refactoring.

--- src/main/java/edu/princeton/cs/algs4/graphs/mst/Edge.java
+++ src/main/java/edu/princeton/cs/algs4/graphs/mst/Edge.java
@@ -86,21 +86,6 @@ public class Edge  {
     }
 
     /**
-     * Compares two edges by weight.
-     * Note that {@code compareTo()} is not consistent with {@code equals()},
-     * which uses the reference equality implementation inherited from {@code Object}.
-     *
-     * @param  that the other edge
-     * @return a negative integer, zero, or positive integer depending on whether
-     *         the weight of this is less than, equal to, or greater than the
-     *         argument edge
-     */
-//    @Override
-//    public int compareTo(Edge that) {
-//        return Double.compare(this.weight, that.weight);
-//    }
-
-    /**
      * Returns a string representation of this edge.
      *
      * @return a string representation of this edge
