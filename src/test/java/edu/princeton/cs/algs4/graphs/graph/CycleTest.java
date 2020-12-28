dad7580d72bf5af6e578cfa47ff92021e254c89d
Jefferson Song <jeffersonsong@yahoo.com>
2020-12-28 11:30:27 -0500

    Add tests.

--- src/test/java/edu/princeton/cs/algs4/graphs/graph/CycleTest.java
+++ src/test/java/edu/princeton/cs/algs4/graphs/graph/CycleTest.java
@@ -1,17 +1,28 @@
 package edu.princeton.cs.algs4.graphs.graph;
 
+import edu.princeton.cs.algs4.graphs.graph.impl.GraphImpl;
 import edu.princeton.cs.algs4.graphs.graph.impl.GraphReader;
 import edu.princeton.cs.algs4.utils.io.In;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CycleTest {
     @Test
-    public void test() {
+    public void testCycle() {
         In in = new In("src/test/resources/41graph/tinyG.txt");
         Graph<Edge> G = GraphReader.readGraph(in);
         Cycle<Edge> finder = new Cycle<>(G);
         assertTrue(finder.hasCycle());
+    }
+
+    @Test
+    public void testNoCycle() {
+        Graph<Edge> G = new GraphImpl<>(3, false);
+        G.addEdge(0, new UnWeightedEdge(0, 1));
+        G.addEdge(1, new UnWeightedEdge(1, 2));
+        Cycle<Edge> finder = new Cycle<>(G);
+        assertFalse(finder.hasCycle());
     }
 }
