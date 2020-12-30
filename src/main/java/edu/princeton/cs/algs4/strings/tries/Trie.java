ac7709fbde05d353ba0035d63f86fec6560c6f78
Jefferson Song <jeffersonsong@yahoo.com>
2020-12-30 17:58:08 -0500

    Add comments.

--- src/main/java/edu/princeton/cs/algs4/strings/tries/Trie.java
+++ src/main/java/edu/princeton/cs/algs4/strings/tries/Trie.java
@@ -2,10 +2,51 @@ package edu.princeton.cs.algs4.strings.tries;
 
 import edu.princeton.cs.algs4.searching.st.ST;
 
+/**
+ *  The {@code TrieST} class represents an symbol table of key-value
+ *  pairs, with string keys and generic values.
+ *  It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
+ *  <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
+ *  It also provides character-based methods for finding the string
+ *  in the symbol table that is the <em>longest prefix</em> of a given prefix,
+ *  finding all strings in the symbol table that <em>start with</em> a given prefix,
+ *  and finding all strings in the symbol table that <em>match</em> a given pattern.
+ *  A symbol table implements the <em>associative array</em> abstraction:
+ *  when associating a value with a key that is already in the symbol table,
+ *  the convention is to replace the old value with the new value.
+ *  Unlike {@link java.util.Map}, this class uses the convention that
+ *  values cannot be {@code null}—setting the
+ *  value associated with a key to {@code null} is equivalent to deleting the key
+ *  from the symbol table.
+ *  <p>
+ *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/52trie">Section 5.2</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ */
 public interface Trie<Value> extends ST<String, Value> {
+    /**
+     * Returns all of the keys in the set that start with {@code prefix}.
+     * @param prefix the prefix
+     * @return all of the keys in the set that start with {@code prefix},
+     *     as an iterable
+     */
     Iterable<String> keysWithPrefix(String prefix);
 
+    /**
+     * Returns all of the keys in the symbol table that match {@code pattern},
+     * where . symbol is treated as a wildcard character.
+     * @param pattern the pattern
+     * @return all of the keys in the symbol table that match {@code pattern},
+     *     as an iterable, where . is treated as a wildcard character.
+     */
     Iterable<String> keysThatMatch(String pattern);
 
+    /**
+     * Returns the string in the symbol table that is the longest prefix of {@code query},
+     * or {@code null}, if no such string.
+     * @param query the query string
+     * @return the string in the symbol table that is the longest prefix of {@code query},
+     *     or {@code null} if no such string
+     * @throws IllegalArgumentException if {@code query} is {@code null}
+     */
     String longestPrefixOf(String query);
 }
